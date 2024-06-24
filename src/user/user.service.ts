a19e2f5fd187cfa021695aaf6c436d70c9780a69
Eiad-Sorour <eiad.sorour@yahoo.com>
2024-06-24 04:01:36 +0300

    userService_updated

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/sequelize";
 import { User } from "./user.model";
 import { UserRegisterDto } from "src/auth/dto/userRegister.dto";
@@ -7,11 +7,7 @@ import { UserRegisterDto } from "src/auth/dto/userRegister.dto";
 export class UserService{
     constructor(@InjectModel(User) private readonly userModel:typeof User){}
 
-    async addUser(userRegisterDto: UserRegisterDto): Promise<void>{
-        try{
-            await this.userModel.create(userRegisterDto as any);
-        }catch(error){
-            throw new HttpException(error.message , 501);
-        }
+    async addUser(userRegisterDto: UserRegisterDto): Promise<User>{
+        return await this.userModel.create(userRegisterDto as any);
     }
 }
