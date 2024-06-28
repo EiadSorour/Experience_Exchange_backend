91c643ec688be2e1d27655e8a33906d5ae09363f
Eiad-Sorour <eiad.sorour@yahoo.com>
2024-06-28 23:21:17 +0300

    set_default_user_role_in_authService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -21,6 +21,7 @@ export class AuthService{
         }
         
         userRegisterDto.password = await bcrypt.hash(userRegisterDto.password , 10);
+        userRegisterDto.role = "client";
         const user = await this.userService.addUser(userRegisterDto);
 
         const payload = {   id: user.userID , username: user.username , role: user.role , 
