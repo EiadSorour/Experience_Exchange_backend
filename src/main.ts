898d21765a7c0297b30c876afde4a57a86542bcf
Eiad-Sorour <eiad.sorour@yahoo.com>
2024-06-29 00:34:41 +0300

    swagger_installed_&_initialized

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppExceptionFilter } from './utils/app.ExceptionFilter';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AppExceptionFilter())
   app.useGlobalPipes(new ValidationPipe({whitelist:true}))
+
+  const config = new DocumentBuilder()
+  .setTitle('Experience Exchange App')
+  .setDescription('Experience exchange app API description')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3001);
 }
 bootstrap();
