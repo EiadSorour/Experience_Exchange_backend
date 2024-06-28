874524d3a505a561e1779bf88ccddb20b27b4eef
Eiad-Sorour <eiad.sorour@yahoo.com>
2024-06-29 01:03:44 +0300

    userLoginDto_userRegisterDto_modified

--- src/auth/dto/userLogin.dto.ts
+++ src/auth/dto/userLogin.dto.ts
@@ -1,11 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class UserLoginDto{
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({
+        required: true,
+        type: String,
+    })
     username: string;
-
+    
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({
+        required: true,
+        type: String,
+    })
     password: string
 }
--- src/auth/dto/userRegister.dto.ts
+++ src/auth/dto/userRegister.dto.ts
@@ -1,17 +1,35 @@
-import { IsEmpty, IsIn, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class UserRegisterDto{
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({
+        required: true,
+        type: String,
+    })
     username: string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({
+        required: true,
+        type: String,
+    })
     profession: string
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({
+        required: true,
+        type: String,
+    })
     password: string
 
+    @ApiProperty({
+        readOnly: true,
+        required: false,
+        enum: ["admin", "client"]
+    })
     role: string;
 }
