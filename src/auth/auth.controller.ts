19a689d368047bdcbb59320b1b2cedd82685839a
Eiad-Sorour <eiad.sorour@yahoo.com>
2024-06-28 23:45:49 +0300

    login_endpoint_implemented_in_authController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { UserRegisterDto } from "./dto/userRegister.dto";
 import { HttpStatusMessage } from "src/utils/httpStatusMessage.enum";
+import { UserLoginDto } from "./dto/userLogin.dto";
 
 @Controller("/api")
 export class AuthController{
@@ -11,6 +12,13 @@ export class AuthController{
     @HttpCode(HttpStatus.OK)
     async register(@Body() userRegisterDto:UserRegisterDto){
         const token = await this.authService.register(userRegisterDto);
+        return {status: HttpStatusMessage.SUCCESS , data: {token}}
+    }
+
+    @Post("/login")
+    @HttpCode(HttpStatus.OK)
+    async login(@Body() userLoginDto:UserLoginDto){
+        const token = await this.authService.login(userLoginDto);
         return {status: HttpStatusMessage.SUCCESS , data: {token}}
     }
 }
