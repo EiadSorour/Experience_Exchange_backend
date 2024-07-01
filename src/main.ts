e264fcb3f94e3ac61a87810796695dd5ff7014c7
Eiad-Sorour <eiad.sorour@yahoo.com>
2024-07-01 05:03:17 +0300

    cors_enabled

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AppExceptionFilter())
   app.useGlobalPipes(new ValidationPipe({whitelist:true}))
+  app.enableCors();
 
   const config = new DocumentBuilder()
   .setTitle('Experience Exchange App')
