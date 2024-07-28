b36bb9aee4873ba9883b57c4081efcc94275750a
Eiad-Sorour <eiad.sorour@yahoo.com>
2024-07-29 01:15:58 +0300

    bug_fixed

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -1,14 +1,14 @@
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { Room } from "./room.model";
-import { UserService } from "src/user/user.service";
 import { RoomController } from "./room.controller";
 import { RoomService } from "./room.service";
+import { UserModule } from "src/user/user.module";
 
 @Module({
     imports: [
         SequelizeModule.forFeature([Room]),
-        UserService
+        UserModule
     ],
     controllers: [RoomController],
     providers: [RoomService],
