a64d417bfcb84a0bdd00d9965e4915301d48a592
Eiad-Sorour <eiad.sorour@yahoo.com>
2024-07-29 00:01:36 +0300

    getRoomsByUsername_service_implemented

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { Room } from "./room.model";
+import { UserService } from "src/user/user.service";
 
 @Module({
-    imports: [SequelizeModule.forFeature([Room])],
+    imports: [
+        SequelizeModule.forFeature([Room]),
+        UserService
+    ],
     controllers: [],
     providers: [],
     exports: []
--- src/room/room.service.ts
+++ src/room/room.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/sequelize";
 import { Room } from "./room.model";
+import { UserService } from "src/user/user.service";
+import { User } from "src/user/user.model";
 
 @Injectable()
 export class RoomService{
-    constructor(@InjectModel(Room) private readonly roomModel: typeof Room){}
+    constructor(
+        @InjectModel(Room) private readonly roomModel: typeof Room,
+        private readonly userService:UserService
+    ){}
 
     async getAllRooms(limit:number , offset:number): Promise<{rows:Room[], count:number}>{
         return await this.roomModel.findAndCountAll({limit:limit , offset:offset , order:[["topic" , "ASC"]] });
@@ -13,9 +18,18 @@ export class RoomService{
     async getAllRoomsByCreatorID(creatorID: string, limit:number , offset:number): Promise<{rows:Room[], count:number}>{
         return await this.roomModel.findAndCountAll({where:{creatorID:creatorID},limit:limit , offset:offset , order:[["topic" , "ASC"]] });
     }
-
+    
     async getAllRoomsByTopic(topic: string, limit:number , offset:number): Promise<{rows:Room[], count:number}>{
         return await this.roomModel.findAndCountAll({where:{topic:topic},limit:limit , offset:offset , order:[["topic" , "ASC"]] });
+    }
+    
+    async getAllRoomsByUsername(username: string, limit:number , offset:number): Promise<{rows:Room[], count:number}>{
+        const user:User = await this.userService.getUserByExactUsername(username);
+        if(!user){
+            return {rows:[] , count:0};
+        }else{
+            return await this.roomModel.findAndCountAll({where:{creatorID:user.id},limit:limit , offset:offset , order:[["topic" , "ASC"]] });
+        }
     }
 
 }
