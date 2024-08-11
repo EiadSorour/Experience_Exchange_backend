08cb584cf701c788ca2ebf474103e9a551e3f222
Your Name <you@example.com>
2024-08-11 22:13:49 +0300

    socketGurad_added

--- src/websocket/websocket.gateway.ts
+++ src/websocket/websocket.gateway.ts
@@ -1,9 +1,13 @@
+import { UseGuards } from "@nestjs/common";
 import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Socket, Server } from "socket.io";
+import { SocketGuard } from "src/guards/socket.guard";
 
+@UseGuards(SocketGuard) 
 @WebSocketGateway(80 , {
     namespace:"/rooms" ,
     cors: {
+        credentials: true,
         origin: process.env.FRONT_ORIGIN,
         methods: ["GET", "POST" , "DELETE" , "PATCH" , "UPDATE"]
     }
@@ -16,10 +20,24 @@ export class WebsocketGateway implements OnGatewayInit, OnGatewayConnection, OnG
     afterInit(server:Server){ 
     }
 
-    handleConnection(client:Socket) {
-        console.log(`Client ${client.id} is connected`);
+    handleConnection(client:Socket) {  
+        console.log(`Client ${client.id} is connected`); 
     }
 
     handleDisconnect(client:Socket) {
     }
+
+    // @SubscribeMessage("sendMessage")
+    // async handleNewMessage(@MessageBody() messageBody:SendMessageDto , @ConnectedSocket() client: Socket){
+    //     const chatID = String(messageBody.chatID);
+    //     const messageText = messageBody.message;
+    //     const username = client.data.payload.username;
+    //     const message = {
+    //         senderUsername: username,
+    //         text: messageText,
+    //         chatID: chatID
+    //     }
+    //     await this.eventsService.saveMessage(message as any);
+    //     client.to(chatID).emit("newMessage" , messageText);
+    // }
 }
